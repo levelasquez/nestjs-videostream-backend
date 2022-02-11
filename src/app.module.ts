05c75e332c83db37314a96b321b067200143da51
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2022-02-11 11:57:45 -0300

    user authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path/posix';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { secret } from './utils/constants';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/stream')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
